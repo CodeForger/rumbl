713fd6225b33c73f0bbbca10bc2f159f97ea4fa4
Ian Zhang <yingzhang.ian@gmail.com>
2017-11-01 21:28:48 +0800

    add user and fake in-memory repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,11 +1,32 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  # use Ecto.Repo, otp_app: :rumbl
 
   @doc """
   Dynamically loads the repository url from the
   DATABASE_URL environment variable.
   """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  # def init(_, opts) do
+  #   {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  # end
+
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
+      %Rumbl.User{id: "2", name: "Ian", username: "ianzhang", password: "mark"},
+      %Rumbl.User{id: "3", name: "Jose", username: "chrismccord", password: "phx"},
+    ]
   end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
+  
 end
